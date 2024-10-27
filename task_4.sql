bbabb563b63e7dff732f4d56ccc2cc4a33c8da23
EslamElaidy <eslam.ayde@gmail.com>
2024-10-28 02:15:29 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,11 @@
-USE alx_book_store;
-SHOW * FROM alx_book_store;
+SELECT 
+(
+COLUMN_NAME,
+COLUMN_TYPE,
+TABLE_SCHEMA = 'alx_book_store', TABLE_NAME = Books
+  )
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
