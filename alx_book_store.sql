7381d2a83843e1ea2228ece1fdc77d6ad6d8ea92
EslamElaidy <eslam.ayde@gmail.com>
2024-10-28 00:48:56 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE TABLE Books (book_id (Primary Key)
-title VARCHAR(130)
-author_id (Foreign Key referencing Authors table)
-price DOUBLE
+CREATE TABLE Books (book_id INT Primary Key,
+title VARCHAR(130),
+author_id (Foreign Key referencing Authors table),
+price DOUBLE,
 publication_date DATE
 );
 
-CREATE TABLE Authors (author_id (Primary Key)
+CREATE TABLE Authors (author_id INT Primary Key,
 author_name VARCHAR(215)
 );
