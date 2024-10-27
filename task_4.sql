ea2a100b4bad5f8861ac5561c456c128f012b125
EslamElaidy <eslam.ayde@gmail.com>
2024-10-28 02:16:13 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,10 @@
 SELECT 
 (
+INFORMATION_SCHEMA.COLUMNS,
 COLUMN_NAME,
 COLUMN_TYPE,
-TABLE_SCHEMA = 'alx_book_store', TABLE_NAME = Books
+TABLE_SCHEMA = 'alx_book_store',
+TABLE_NAME = Books
   )
 FROM 
     information_schema.COLUMNS
