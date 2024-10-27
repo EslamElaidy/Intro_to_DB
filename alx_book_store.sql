0ddaa03ec92d42137cb543f4e5034ab226185e08
EslamElaidy <eslam.ayde@gmail.com>
2024-10-28 00:52:02 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -10,3 +10,13 @@ publication_date DATE
 CREATE TABLE Authors (author_id INT Primary Key,
 author_name VARCHAR(215)
 );
+CREATE TABLE Customers (customer_id INT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT,
+);
+
+CREATE TABLE Orders(order_id INT PRIMARY KEY,
+customer_id (Foreign Key referencing Customers table),
+order_date DATE
+  )
