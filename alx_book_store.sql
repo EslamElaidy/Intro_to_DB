904db2b4f6482ac16fa05c64dc1514384edd321f
EslamElaidy <eslam.ayde@gmail.com>
2024-10-28 00:39:48 +0300

    Create alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Books ( book_id (Primary Key)
+title VARCHAR(130)
+author_id (Foreign Key referencing Authors table)
+price DOUBLE
+publication_date DATE
+);
+
+CREATE TABLE Authors (author_id (Primary Key)
+author_name VARCHAR(215)
+);
